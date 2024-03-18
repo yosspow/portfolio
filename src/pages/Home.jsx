3a70e3a9ed26c05437c701d3e3002c5e85a447aa
yosspow <elhabachi.youssef@arkx.academy>
2024-03-18 03:33:04 +0000

    push code

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+
+import Projects from '../components/Projects';
+import Testimonial from '../components/Testimonial';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Service from '../components/Service';
+import HomePagdData from '../data/HomePagdData.json';
+import Experience from '../components/Experience';
+import Contact from '../components/Contact';
+
+export default function Home() {
+  const {
+    hero,
+    socialBtns,
+    about,
+    projects,
+    experience,
+    contact,
+  } = HomePagdData;
+  return (
+    <>
+      <Hero data={hero} socialData={socialBtns} />
+
+      <About data={about} />
+      <Projects data={projects} />
+      <Experience data={experience} />
+   
+      <Contact data={contact} socialData={socialBtns} />
+    </>
+  );
+}
